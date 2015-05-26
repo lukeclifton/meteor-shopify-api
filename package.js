fb84bef78b759bb1435b6862bc49b9b86edb1490
Luke Clifton <luke@lionsorbet.com>
2015-05-26 14:40:05 +0100

    Forgot to remove temp local package name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-	name: 'cliffers:shopify-api-local',
+	name: 'cliffers:shopify-api',
 	summary: 'Package for the Shopify API. Also handles OAuth authentication for embedded shopify apps',
 	version: '0.3.0',
 	git: 'https://github.com/lukeclifton/shopify-api-meteor'
